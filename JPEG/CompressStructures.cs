793fc821b4632130f1b56edec2aa0e61ce09f521
GH2OOST <38152065+GH2OOST@users.noreply.github.com>
2019-03-18 19:24:42 +0500

    MathExFix

---
+++ JPEG/CompressStructures.cs
@@ -0,0 +1,18 @@
+﻿namespace JPEG
+{
+    public class CompressStructures
+    {
+        public byte[] QuantizedBytes;
+        public double[,] ChannelFreqs;
+        public double[,] SubMatrix;
+        public byte[,] QuantizedFreqs;
+
+        public CompressStructures()
+        {   
+            //ChannelFreqs = new double[Program.DCTSize, Program.DCTSize];
+            //SubMatrix = new double[Program.DCTSize, Program.DCTSize];
+            //QuantizedFreqs = new byte[Program.DCTSize, Program.DCTSize];
+            //QuantizedBytes = new byte[Program.DCTSize * Program.DCTSize];
+        }
+    }
+}
